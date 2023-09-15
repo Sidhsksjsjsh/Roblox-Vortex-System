5b267a0db679b6f722486dfd7128bb345fee40c0
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-09-15 16:55:30 +0900

    Update System.lua

--- System.lua
+++ System.lua
@@ -1,9 +1,8 @@
 local Vortex = {}
 local contentprovider = game:GetService("ContentProvider")
-local mt = getrawmetatable(game)
-local oldnc = mt.__namecall
-local oldidx = mt.__index
-
+local HttpService = game:GetService("HttpService")
+--local hint = Instance.new("Hint",game:GetService("Workspace"))
+--[[
 local NotificationBindable = Instance.new("BindableFunction")
 function Msgreq(Title,Text,Duration,Button1Text,Button2Text)
 game.StarterGui:SetCore("SendNotification", {
@@ -24,23 +23,29 @@ if result == "Hide Notify" then
 end
 
 --Msgreq("","",999999999,"Hide Notify","")
+]]
 
 function Vortex:BypassLoadingScreen()
+local mt = getrawmetatable(game)
+local oldnc = mt.__namecall
+local oldidx = mt.__index
+     
 setreadonly(mt, false)
 
 mt.__namecall = newcclosure(function(obj, ...)
    local method = getnamecallmethod()
    if method == "PreloadAsync" and obj == contentprovider then
-        --notify("Bypassed Loading Screen","success makes PreloadAsync wait() duration become 0.1!")
-        Msgreq("Bypassed Loading Screen","success makes PreloadAsync wait() duration become 0.1!",999999999,"Hide Notify","")
-       return wait(0.1)
+        return wait(0.1)
+          --hint.Text = "Successfully bypassed the loading screen."
+          --wait()
+          --hint.Text = ""
    end
    return oldnc(obj, ...)
 end)
 
 mt.__index = newcclosure(function(obj, idx)
    if idx == "RequestQueueSize" and obj == contentprovider then
-        Msgreq("Bypassed Loading Screen","success makes RequestQueueSize duration become 0!",999999999,"Hide Notify","")
+        -- Msgreq("Bypassed Loading Screen","success makes RequestQueueSize duration become 0!",999999999,"Hide Notify","")
        return 0
    end
    return oldidx(obj, idx)
@@ -49,31 +54,16 @@ end)
 setreadonly(mt, true)
 end
 
-function Vortex:AntiHookFunction(tbl,funcName)
-    local originalFunction = tbl[funcName]
-    
-    setmetatable(tbl, {
-        __index = function(_, key)
-            if key == funcName then
-                return originalFunction
-            end
-        end
-        
-        __newindex = function(_, key, value)
-            if key == funcName then
-                --notify("Attempt to overwrite",funcName .. " detected!")
-                Msgreq("Attempt to overwrite",tostring(funcName) .. " detected!",999999999,"Hide Notify","")
-                for _, player in ipairs(game.Players:GetPlayers()) do
-                    if player and player:IsA("Player") then
-                        --notify("Hookfunction Detected!","Player " .. player.Name .. " is suspected of attempting to use hookfunction!")
-                        Msgreq("Hookfunction Detected!","Player " .. tostring(player.Name) .. " is suspected of attempting to use hookfunction!",999999999,"Hide Notify","")
-                    end
-                end
-                return
-            end
-            rawset(tbl, key, value)
-        end
-    })
-end
+function Vortex:GUID(ignorelist,GUIDtoggle)
+ local GUID = HttpService:GenerateGUID(GUIDtoggle)
 
--- protectFunctionWithPrint(game, "SomeFunction")
+     if ignorelist == "-" then
+          return GUID:gsub("-","")
+     elseif ignorelist == "abc" then
+          return GUID:gsub("a",""):gsub("b",""):gsub("c",""):gsub("d",""):gsub("e",""):gsub("f",""):gsub("g",""):gsub("h",""):gsub("i",""):gsub("j",""):gsub("k",""):gsub("l",""):gsub("m",""):gsub("n",""):gsub("o",""):gsub("p",""):gsub("q",""):gsub("r",""):gsub("s",""):gsub("t",""):gsub("u",""):gsub("v",""):gsub("w",""):gsub("x",""):gsub("y",""):gsub("z",""):gsub("A",""):gsub("B",""):gsub("C",""):gsub("D",""):gsub("E",""):gsub("F",""):gsub("G",""):gsub("H",""):gsub("I",""):gsub("J",""):gsub("K",""):gsub("L",""):gsub("M",""):gsub("N",""):gsub("O",""):gsub("P",""):gsub("Q",""):gsub("R",""):gsub("S",""):gsub("T",""):gsub("U",""):gsub("V",""):gsub("W",""):gsub("X",""):gsub("Y",""):gsub("Z","")
+     elseif ignorelist == "abc and -" or ignorelist == "- and abc" then
+          return GUID:gsub("a",""):gsub("b",""):gsub("c",""):gsub("d",""):gsub("e",""):gsub("f",""):gsub("g",""):gsub("h",""):gsub("i",""):gsub("j",""):gsub("k",""):gsub("l",""):gsub("m",""):gsub("n",""):gsub("o",""):gsub("p",""):gsub("q",""):gsub("r",""):gsub("s",""):gsub("t",""):gsub("u",""):gsub("v",""):gsub("w",""):gsub("x",""):gsub("y",""):gsub("z",""):gsub("A",""):gsub("B",""):gsub("C",""):gsub("D",""):gsub("E",""):gsub("F",""):gsub("G",""):gsub("H",""):gsub("I",""):gsub("J",""):gsub("K",""):gsub("L",""):gsub("M",""):gsub("N",""):gsub("O",""):gsub("P",""):gsub("Q",""):gsub("R",""):gsub("S",""):gsub("T",""):gsub("U",""):gsub("V",""):gsub("W",""):gsub("X",""):gsub("Y",""):gsub("Z",""):gsub("-","")
+     else
+          return GUID
+     end
+end
