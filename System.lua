daeeb9c93c091677b69d93881cfedb9e87095a1a
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-01-30 17:11:57 +0900

    Update System.lua

--- System.lua
+++ System.lua
@@ -1019,9 +1019,112 @@ else
 	end
 end
 
+function Vortex:Player(str)
+for i,v in pairs(Players:GetPlayers()) do
+      str(i,v)
+end
+end
+
+function Vortex:Children(vsc,str)
+for _,v in pairs(vsc:GetChildren()) do
+	str(v)
+end
+end
+
+function Vortex:Descendants(vsc,str)
+for _,v in pairs(vsc:GetDescendants()) do
+	str(v)
+end
+end
+
 local playerGui = LocalPlayer.PlayerGui
 local screenHeight = workspace.CurrentCamera.ViewportSize.Y -- Tinggi layar
 local screenWidth = workspace.CurrentCamera.ViewportSize.X -- Lebar layar
+
+            local ScreenGui = Instance.new("ScreenGui")
+            local Frame = Instance.new("Frame")
+            local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+            local UICorner = Instance.new("UICorner")
+            local UIListLayout = Instance.new("UIListLayout")
+            local TextLabel = Instance.new("TextLabel")
+            local TextButton = Instance.new("TextButton")
+            local TextButton_2 = Instance.new("TextButton")
+            local UICorner_2 = Instance.new("UICorner")
+            local UICorner_3 = Instance.new("UICorner")
+
+            ScreenGui.Parent = game:GetService("CoreGui");
+            ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+            ScreenGui.Enabled = false
+            ScreenGui.Name = "Adonis Notify"
+
+            Frame.Parent = ScreenGui
+            Frame.BackgroundColor3 = Color3.fromRGB(19, 24, 52)
+            Frame.Position = UDim2.new(0.5, 0, .5, 0)
+            Frame.Size = UDim2.new(.55, 0, .6, 0)
+            Frame.AnchorPoint = Vector2.new(.5,.5)
+
+            UIAspectRatioConstraint.Parent = Frame
+            UIAspectRatioConstraint.AspectRatio = 2.000
+
+            UICorner.CornerRadius = UDim.new(0, 15)
+            UICorner.Parent = Frame
+
+            UIListLayout.Parent = Frame
+            UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
+            UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+            UIListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
+            UIListLayout.Padding = UDim.new(.05, 0)
+
+            TextLabel.Parent = Frame
+            TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            TextLabel.BackgroundTransparency = 1.000
+            TextLabel.Position = UDim2.new(0.5, 0, 0.1, 0)
+            TextLabel.Size = UDim2.new(1, 0, .5, 0)
+            TextLabel.Font = Enum.Font.GothamBlack
+            TextLabel.Text = "WARNING!\nWe found Anti-cheat which is owned by adonis admin\n( Do you want to bypass all anti-cheats? )"
+            TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+            TextLabel.TextSize = 17.000
+            TextLabel.TextWrapped = true
+
+            TextButton.Parent = Frame
+            TextButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+            TextButton.Size = UDim2.new(.5, 0, .15, 0)
+            TextButton.Font = Enum.Font.SourceSans
+            TextButton.Text = "Yes"
+            TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+            TextButton.TextSize = 41.000
+            TextButton.TextWrapped = true
+            
+            TextButton_2.Parent = Frame
+            TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+            TextButton_2.Size = UDim2.new(.5, 0, .15, 0)
+            TextButton_2.Font = Enum.Font.SourceSans
+            TextButton_2.Text = "No"
+            TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+            TextButton_2.TextSize = 41.000
+            TextButton_2.TextWrapped = true
+
+            UICorner_2.CornerRadius = UDim.new(.1, 0)
+            UICorner_2.Parent = TextButton
+            UICorner_3.CornerRadius = UDim.new(.1, 0)
+            UICorner_3.Parent = TextButton_2
+            TextButton.MouseButton1Click:Once(function()
+		Vortex:AdonisBypass("V3")
+		TextLabel.Text = "Bypasser is running, dont close this window.\nthis will automatically close this window."
+		TextButton.Text = "(...)"
+		TextButton_2.Text = "(...)"
+		wait(10)
+		ScreenGui.Enabled = false
+	    end)
+            TextButton_2.MouseButton1Click:Once(function()
+		ScreenGui.Enabled = false
+	    end)
+
+Vortex:Descendants(game:GetService("ReplicatedStorage"),function(id,vr)
+	if vr.Name == "__FUNCTION" or vr.Name == "__FUNCTIONS" then
+		ScreenGui.Enabled = true
+	end
+end)
 
 function Vortex:PlayerImmortal()
 	LocalPlayer.Character:FindFirstChildWhichIsA("Humanoid"):ChangeState(11)
@@ -1483,24 +1586,6 @@ end
 
 function Vortex:WriteCommandPrompt(str)
 	CommandPrompt:RequestLine(str)
-end
-
-function Vortex:Player(str)
-for i,v in pairs(Players:GetPlayers()) do
-      str(i,v)
-end
-end
-
-function Vortex:Children(vsc,str)
-for _,v in pairs(vsc:GetChildren()) do
-	str(v)
-end
-end
-
-function Vortex:Descendants(vsc,str)
-for _,v in pairs(vsc:GetDescendants()) do
-	str(v)
-end
 end
 
 local _L = {}
